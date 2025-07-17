2b2cb23976cc9b818de452c00c70fe4b26979f5b
Va01007 <108982082+Va01007@users.noreply.github.com>
2025-07-17 03:35:25 +0300

    Add install file

---
+++ Install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Create and activate Conda environment
+conda create -y -n CHAOS_MutGBS python=3.9 \
+    bioconda::nextflow \
+    bioconda::bioawk \
+    bioconda::htslib \
+    conda-forge::make \
+    conda-forge::automake \
+    conda-forge::autoconf \
+    conda-forge::libtool \
+    conda-forge::zlib \
+    conda-forge::bzip2 \
+    conda-forge::xz \
+    conda-forge::curl \
+    conda-forge::gcc \
+    -c conda-forge -c bioconda
+
+conda activate CHAOS_MutGBS
+
+# Clone repositories
+git clone https://github.com/Va01007/CHAOS-MutGBS.git
+cd CHAOS-MutGBS
+git clone https://github.com/RAHenriksen/NGSNGS.git
+
+# Build NGSNGS with Conda's htslib
+cd NGSNGS
+make
+
+# Add to PATH within environment
+mkdir -p $CONDA_PREFIX/envs/CHAOS_MutGBS/bin
+cp ngsngs $CONDA_PREFIX/envs/CHAOS_MutGBS/bin/
+cd ../..
+
+echo "Installation complete!"
+echo "Activate environment with: conda activate CHAOS_MutGBS"
+echo "Verify with: ngsngs --help"
